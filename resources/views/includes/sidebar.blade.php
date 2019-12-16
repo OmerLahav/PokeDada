fb4a0b299a76e6fa7429f57ad96cf292797deda8
Omer Lahav <omer@geekmedia.co.il>
2019-12-16 08:33:29 +0200

    edit homepage css + update dependencies versions

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,16 +1,20 @@
 <div class="container">
     <div class="panel panel-default dada-sidebar-box w-100 my-2">
-        <div class="panel-heading bg-red text-white font-weight-bold px-1">Panel Heading</div>
+        <div class="panel-heading bg-red text-white font-weight-bold px-1">Box 1</div>
         <div class="panel-body px-1">Panel Content</div>
     </div>
 
     <div class="panel panel-default dada-sidebar-box w-100 my-2">
-        <div class="panel-heading bg-red text-white font-weight-bold px-1">Panel Heading</div>
+        <div class="panel-heading bg-red text-white font-weight-bold px-1">Box 2</div>
         <div class="panel-body px-1">Panel Content</div>
     </div>
 
     <div class="panel panel-default dada-sidebar-box w-100 my-2">
-      <div class="panel-heading bg-red text-white font-weight-bold px-1">Panel Heading</div>
-      <div class="panel-body px-1">Panel Content</div>
+        <div class="panel-heading bg-red text-white font-weight-bold px-1">Ads</div>
+        <div class="panel-body px-1">
+            <div
+                style="width: 100%; height: 250px; background: lightgray; color: #000; line-height: 250px; text-align: center; ">
+                GENERIC AD</div>
+        </div>
     </div>
 </div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,9 +6,14 @@
     <div class="panel-body px-1">Dada's huge penis is well-known all across China.</div>
 </div>
 
+<div class="setHeadline mx-auto">
+<h3>Sun & Moon Set</h3>
+<hr class="styleHrHeadline">
+</div>
+
 <div class="cards-container">
     <div class="card col-sm-6 col-lg-3">
-        <img class="card-img-top pokemon-img" src="{{ asset('img/eevee.png') }}" alt="Card image">
+        <img class="card-img-top pokemon-img" src="{{ asset('img/backcard.jpg') }}" alt="Card image">
         <div class="card-body">
             <h4 class="card-title">John Doe</h4>
             <p class="card-text">Some example text.</p>
@@ -17,30 +22,119 @@
     </div>
 
     <div class="card col-sm-6 col-lg-3">
-        <img class="card-img-top pokemon-img" src="{{ asset('img/eevee.png') }}" alt="Card image">
+        <img class="card-img-top pokemon-img" src="{{ asset('img/backcard.jpg') }}" alt="Card image">
         <div class="card-body">
-          <h4 class="card-title">John Doe</h4>
-          <p class="card-text">Some example text.</p>
-          <a href="#" class="btn btn-primary">See Profile</a>
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn btn-primary">See Profile</a>
         </div>
     </div>
 
     <div class="card col-sm-6 col-lg-3">
-        <img class="card-img-top pokemon-img" src="{{ asset('img/eevee.png') }}" alt="Card image">
+        <img class="card-img-top pokemon-img" src="{{ asset('img/backcard.jpg') }}" alt="Card image">
         <div class="card-body">
-          <h4 class="card-title">John Doe</h4>
-          <p class="card-text">Some example text.</p>
-          <a href="#" class="btn btn-primary">See Profile</a>
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn btn-primary">See Profile</a>
         </div>
     </div>
 
- <div class="card col-sm-6 col-lg-3">
-     <img class="card-img-top pokemon-img" src="{{ asset('img/eevee.png') }}" alt="Card image">
-     <div class="card-body">
-       <h4 class="card-title">John Doe</h4>
-       <p class="card-text">Some example text.</p>
-       <a href="#" class="btn btn-primary">See Profile</a>
-     </div>
- </div>
+    <div class="card col-sm-6 col-lg-3">
+        <img class="card-img-top pokemon-img" src="{{ asset('img/backcard.jpg') }}" alt="Card image">
+        <div class="card-body">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+    </div>
 </div>
+
+<!-- Second Row -->
+<div class="setHeadline mx-auto">
+<h3>Sword & Shield Set</h3>
+<hr class="styleHrHeadline">
+</div>
+
+<div class="cards-container">
+    <div class="card col-sm-6 col-lg-3">
+        <img class="card-img-top pokemon-img" src="{{ asset('img/backcard.jpg') }}" alt="Card image">
+        <div class="card-body">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+    </div>
+
+    <div class="card col-sm-6 col-lg-3">
+        <img class="card-img-top pokemon-img" src="{{ asset('img/backcard.jpg') }}" alt="Card image">
+        <div class="card-body">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+    </div>
+
+    <div class="card col-sm-6 col-lg-3">
+        <img class="card-img-top pokemon-img" src="{{ asset('img/backcard.jpg') }}" alt="Card image">
+        <div class="card-body">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+    </div>
+
+    <div class="card col-sm-6 col-lg-3">
+        <img class="card-img-top pokemon-img" src="{{ asset('img/backcard.jpg') }}" alt="Card image">
+        <div class="card-body">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+    </div>
+</div>
+
+<!-- Third Row -->
+<div class="setHeadline mx-auto">
+<h3>Random Set</h3>
+<hr class="styleHrHeadline">
+</div>
+
+<div class="cards-container">
+    <div class="card col-sm-6 col-lg-3">
+        <img class="card-img-top pokemon-img" src="{{ asset('img/backcard.jpg') }}" alt="Card image">
+        <div class="card-body">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+    </div>
+
+    <div class="card col-sm-6 col-lg-3">
+        <img class="card-img-top pokemon-img" src="{{ asset('img/backcard.jpg') }}" alt="Card image">
+        <div class="card-body">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+    </div>
+
+    <div class="card col-sm-6 col-lg-3">
+        <img class="card-img-top pokemon-img" src="{{ asset('img/backcard.jpg') }}" alt="Card image">
+        <div class="card-body">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+    </div>
+
+    <div class="card col-sm-6 col-lg-3">
+        <img class="card-img-top pokemon-img" src="{{ asset('img/backcard.jpg') }}" alt="Card image">
+        <div class="card-body">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+    </div>
+</div>
+
 @stop
